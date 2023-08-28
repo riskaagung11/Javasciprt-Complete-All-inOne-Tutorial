a2a8b20f9d14a30f8079c17238c22511be2e8541
Riska Agung <riskaagung11@gmail.com>
2023-08-28 10:32:02 +0700

    Chapter 4

---
+++ 04 Math Methods/js/main.js
@@ -0,0 +1,11 @@
+// Math Methods and Properties
+console.log(Math.PI);
+console.log(Math.trunc(Math.PI));
+console.log(Math.round(4.3));
+console.log(Math.ceil(3.3));
+console.log(Math.floor(6.8));
+console.log(Math.pow(2, 4));
+console.log(Math.min(2, 3, 4, 5, 6, 1, 8, 0.9));
+console.log(Math.max(2, 3, 4, 5, 6, 1, 8, 0.9));
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1);
