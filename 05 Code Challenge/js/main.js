472dc2d5fef1aafbf18bbde8b3efb05a38975ac1
Riska Agung <riskaagung11@gmail.com>
2023-08-28 18:43:04 +0700

    Chapter 5

---
+++ 05 Code Challenge/js/main.js
@@ -0,0 +1,8 @@
+// Your First Code Challenge
+
+// Tulis kode yang akan mengembalikan huruf dari nama Anda
+console.log("Riska".charAt(Math.floor(Math.random() * 5)));
+
+const anyName = "Budi";
+
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
